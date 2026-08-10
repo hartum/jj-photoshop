6f2d02b917b71e8c14dc585e7d5b231551dbb5c6
Hartum <hartum@gmail.com>
2026-08-10 08:58:09 +0200

    feat: configure dev server proxy to run App in localhost

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,15 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
